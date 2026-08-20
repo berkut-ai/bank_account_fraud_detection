d30abdb004d6135bcf5f4162c016b2affe9b854e
root <>
2026-08-20 15:47:29 +0200

    added prediction for file data

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, Body, Request
-from inference import predict
+from fastapi import FastAPI, Body, Request, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from inference import predict, predict_file
+from io import BytesIO, StringIO
 from typing import Annotated
 from schemas import PredictResponse, PredictRequest
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
-
+import pandas as pd
 
 app = FastAPI()
 
@@ -23,4 +25,28 @@ async def root(request: Request):
 @app.post("/v1/predict", response_model=PredictResponse)
 @limiter.limit("30/minute")
 async def create_predict(request: Request, data: Annotated[PredictRequest, Body()]):
-    return predict(data)
+    return PredictResponse(**predict(data.model_dump()))
+
+@app.post("/v1/predict-file")
+@limiter.limit("20/minute")
+async def create_predict_file(request: Request, file: UploadFile):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="File should have .csv extension.")
+
+    content = await file.read()
+
+    try:
+        df = pd.read_csv(BytesIO(content))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Failed to read the file: {e}")
+
+    results = predict_file(df)
+    output = StringIO()
+    results.to_csv(output, index=False)
+    output.seek(0)
+
+    return StreamingResponse(
+        output,
+        media_type="text/csv",
+        headers={"Content-Disposition": 'attachment; filename="predictions.csv"'}
+    )
